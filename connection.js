79a56bba6420381f9b3c45e5540d5fbec2939073
Krishna Kumar <krishnakumarkeer5017@gmail.com>
2022-07-08 14:14:10 +0530

    Updated database

--- connection.js
+++ connection.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
-mongoose.connect("mongodb://localhost:27017/mortalitybaseddatabase")
-.then(console.log("connected to Database"))
-.catch((err) => console.log(err));
+// mongoose.connect("mongodb://localhost:27017/mortalitybaseddatabase")
+// .then(console.log("connected to Database"))
+// .catch((err) => console.log(err));
 
-// const uri = process.env.MongoURL;
-// mongoose.connect(uri,{
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true,
-// }).then(()=>{
-//     console.log("Connected to Database");
-// }).catch((err) =>console.log(err));
+const uri = process.env.MongoURL;
+mongoose.connect(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("Connected to Database");
+}).catch((err) =>console.log(err));
 
 module.exports = mongoose;
